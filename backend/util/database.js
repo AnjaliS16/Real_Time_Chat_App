b34d7ca7608fe14826e4f4945455733a58514964
AnjaliS16 <154319674+AnjaliS16@users.noreply.github.com>
2024-06-08 09:47:08 -0700

    Add files via upload

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-
+const mysql2 = require('mysql2');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     dialect: 'mysql', 
-    host: process.env.DB_HOST
+    host: process.env.DB_HOST,
+    dialectModule: mysql2
 });
 
 module.exports = sequelize;
